c67d5c77598f17be3936df09f313fb7878fe371e
withstars <15535822816@163.com>
2017-10-07 11:18:45 +0800

    [第一个Demo:快速开始]

--- spring-boot-library-2/src/main/java/com/withstars/dao/BookDao.java
+++
@@ -1,9 +0,0 @@
-package com.withstars.dao;
-
-import com.withstars.domain.Book;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookDao {
-    Book getBookById(Long bookId);
-}
--- spring-boot-library-2/src/main/java/com/withstars/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.withstars.service;
-
-import com.withstars.domain.Book;
-
-public interface BookService {
-    Book getBookById(Long bookId);
-}
